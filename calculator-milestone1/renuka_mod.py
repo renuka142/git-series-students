0d72bbd2b56b8e794c3ce30ceb5c6b11f4594f79
Renuka Sanjeev Bhure <72491412+renuka142@users.noreply.github.com>
2020-12-11 17:46:25 +0530

    updating the mod function code

--- calculator-milestone1/renuka_mod.py
+++ calculator-milestone1/renuka_mod.py
@@ -1,6 +1,4 @@
-def mod(x,y): 
-    z=x%y 
-    return z
-x=int(input("enter first number : "))
-y=int(input("enter second number : "))
-print("mod : ",mod(x,y))
+#function to find mod of two numbers
+def mod(a,b):
+    c=a%b
+    return c
