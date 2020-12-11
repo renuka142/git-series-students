96b14f61c5ae4489c6fe3662e8dad1bf7f18b07f
Pranali Mate <pranalimate2713@gmail.com>
2020-12-11 17:04:34 +0530

    Adding substract function

--- calculator-milestone1/sindhu_add.py
+++ calculator-milestone1/sindhu_add.py
@@ -3,10 +3,4 @@ def  calc_add(a,b):
     c = a+b
     return c
 
-#Taking  a and b as inputs
-a = int(input("Enter value of a : "))
-b = int(input("Enter value of b : "))
 
-#get and display result
-result = calc_add(a,b)
-print(result)
